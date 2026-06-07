8e5d3b41bbb68bca2e9a0f097beed2ce2fc0e2cc
batvin3211 <104458965+batvin3211@users.noreply.github.com>
2026-06-07 10:21:00 -0400

    Update base.nix

--- configuration/base.nix
+++ configuration/base.nix
@@ -152,6 +152,12 @@
     ];
   };
 
+  programs.steam = {
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   programs.captive-browser.enable = true;
   programs.captive-browser.bindInterface = false;
 
