3aea106f234d17b4a4f9e47443a682db0e4b446b
vincent <vincent@ascosilinux.com>
2026-06-09 14:55:42 -0400

    added proton-ge

--- configuration/base.nix
+++ configuration/base.nix
@@ -156,6 +156,7 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
 
   programs.captive-browser.enable = true;
